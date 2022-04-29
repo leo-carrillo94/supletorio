0451ad8565db702748af3d59d6cd9602f48c3e28
Leonel Alberto Carrillo Contreras <leonelalbertocc@ufps.edu.co>
2022-04-29 01:05:33 -0500

    se ajusto la fecha y algunos detalles

--- js/democracia.js
+++ js/democracia.js
@@ -87,10 +87,17 @@ function obtenerEndPointTecnologia(){
                 xhttp3.send();
     
             }
+            function insertarFecha(){
+            
+                var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+                var f=new Date();
+                document.getElementById("fecha").innerHTML= `${f.getDate()} de ${meses[f.getMonth()]} de ${f.getFullYear()}`;
+                document.write(f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
+            }
     
     
     
-    
+            insertarFecha();
             obtenernoticia();
             obtenerEndPointDeporte();
             obtenerEndPointTecnologia();
